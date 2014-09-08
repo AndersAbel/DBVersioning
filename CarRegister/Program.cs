d6dfea185be500fa8e08edcd5bc562e91b4533f6
Anders Abel <anders@abel.nu>
2014-09-08 21:21:23 +0200

    Working Main.

--- CarRegister/Program.cs
+++ CarRegister/Program.cs
@@ -10,6 +10,35 @@ namespace CarRegister
     {
         static void Main(string[] args)
         {
+            Person p = new Person();
+
+            Console.Write("Förnamn: ");
+            p.FirstName = Console.ReadLine();
+            Console.Write("Efternamn: ");
+            p.LastName = Console.ReadLine();
+
+            Console.Write("Antal bilar: ");
+            int carCount = int.Parse(Console.ReadLine());
+
+            p.Cars = new List<Car>();
+
+            for (int i = 0; i < carCount; i++)
+            {
+                Console.Write("RegNo: ");
+                p.Cars.Add(new Car() { RegNo = Console.ReadLine() });
+            }
+
+            using (CarRegisterContext ctx = new CarRegisterContext())
+            {
+                ctx.People.Add(p);
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("{0} {1} sparad med {2} bilar",
+                p.FirstName, p.LastName, carCount);
+
+            Console.WriteLine("Tryck på någon tangent för att fortsätta...");
+            Console.ReadKey();
         }
     }
 }
