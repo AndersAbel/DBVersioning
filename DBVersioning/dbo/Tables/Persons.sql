16553a80e4dcc1023c8f19bd1169379ec803722c
Anders Abel <anders@abel.nu>
2014-09-08 20:04:01 +0200

    Added address field.

--- DBVersioning/dbo/Tables/Persons.sql
+++ DBVersioning/dbo/Tables/Persons.sql
@@ -2,6 +2,7 @@
     [Id]          INT           NOT NULL,
     [FirstName]   NVARCHAR (20) NOT NULL,
     [LastName]    NVARCHAR (20) NOT NULL,
+    [Address]     NVARCHAR (50) NOT NULL,
     [PhoneNumber] VARCHAR (20)  NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
